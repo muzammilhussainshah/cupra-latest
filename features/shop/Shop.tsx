0bff7ab33e90a1a560b3b07ef3cc9d9727f3684b
Muzammil shah <mynameismuzammilhussainshah@gmail.com>
2021-09-02 14:57:10 +0500

    shop search fixed [UPDATE]

--- features/shop/Shop.tsx
+++ features/shop/Shop.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import { FlatList, View, Text, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { SHOPSUBCATOGERY, } from "../../store/constant/constant";
 
 import { Header } from '../../components/Header';
 import { DrawerActions } from '@react-navigation/native';
@@ -11,24 +12,26 @@ import { StaticShop } from '../../data/StaticShop';
 
 import { HeaderTitle, ShopContainer, SubCategoryTile } from './ShopStyled';
 
-import { _getCatogery, _getSubCatogery, _getItemDetails } from '../../store/action/shopAction';
+import { _getCatogery, _getSubCatogery, _getItemDetails, _loading } from '../../store/action/shopAction';
 
 import { useDispatch, useSelector } from 'react-redux';
 
 import { Value } from 'react-native-reanimated';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-export const Shop: React.FC = ({ navigation }: any) => {
+export const Shop: React.FC = ({ navigation, }: any) => {
   const [catogery, setcatogery] = useState([]);
 
   const [Subcatogery, setSubcatogery] = useState([]);
 
   const [search, setsearch] = useState([]);
 
+  const [searchedItems, setsearchedItems] = useState([]);
+
   const [getReview, setgetReview] = useState(false);
 
   const [flag, setflag] = useState(false);
-  
+
   const [isEmptyserch, setisEmptyserch] = useState(false);
 
   const dispatch = useDispatch();
@@ -41,23 +44,6 @@ export const Shop: React.FC = ({ navigation }: any) => {
 
   const isLoader = useSelector(({ reducer }: any) => reducer.isLoader);
 
-
-  const searchUser: any = (e: any) => {
-    let keywords = e.split(' ')
-    setsearch(keywords)
-    console.log('working fine')
-    if (keywords[0] === "") {
-      setSubcatogery(shopSubCatogery)
-    }
-    if (keywords[0] !== "") {
-      let searchPattern = new RegExp(keywords.map((term: any) => `(?=.*${term})`).join(''), 'i');
-      let filterChat = [];
-      for (let index = 0; index < shopSubCatogery.length; index++) {
-        filterChat = shopSubCatogery.filter((data: any) => { return data.en_name.match(searchPattern) });
-      }
-      setSubcatogery(filterChat)
-    }
-  }
 
   useEffect(() => {
     dispatch(_getCatogery(currentUser, navigation))
@@ -73,24 +59,66 @@ export const Shop: React.FC = ({ navigation }: any) => {
     setflag(!flag)
   }, [shopSubCatogery])
 
+
+
+
+
+
+
+  function removeDuplicates(data, key) {
+
+    return [
+      ...new Map(data.map(item => [key(item), item])).values()
+    ]
+
+  };
+
+
+  const searchUser: any = async (e: any) => {
+  dispatch(_loading(true))
+    let keywords = e.split(' ')
+    setsearch(keywords)
+    if (keywords[0] === "") {
+      setSubcatogery(shopSubCatogery)
+
+    }
+    if (keywords[0] !== "") {
+      let searchPattern = new RegExp(keywords.map((term: any) => `(?=.*${term})`).join(''), 'i');
+      let filterChat: any = [];
+      let searchedItems: any;
+
+      for (let index = 0; index < shopSubCatogery.length; index++) {
+        // console.log(shopSubCatogery[index], "++++++++++++++++", shopSubCatogery)
+        for (let j = 0; j < shopSubCatogery[index].items.length; j++) {
+          // console.log(shopSubCatogery[j].items, "filterChatfilterChatfilterChat")
+          if (shopSubCatogery[index].items[j].en_name.match(searchPattern)||shopSubCatogery[index].items[j].ar_name.match(searchPattern)) {
+            filterChat.push(shopSubCatogery[index])
+            // console.log("filterChatfilterChatfilterChat", Subcatogery[index])
+          }
+
+        }
+      }
+      // console.log(filterChat, "+++++++++++++++++++")
+      searchedItems = await removeDuplicates(filterChat, item => item._id)
+      setSubcatogery(searchedItems)
+    }
+    dispatch(_loading(false))
+
+  }
+
+
   return (
     <>
       {/* {getReview &&
         <GetReviewComponent _func={() => setgetReview(false)} />
       } */}
       <ShopContainer>
-        {flag ?
-          <Header
+        <Header
           isEmptyserch={isEmptyserch}
-            _func={(e: any) => searchUser(e)}
-            searchBarInput={true}
-            onOpenDrawer={() => navigation.dispatch(DrawerActions.openDrawer())} />
-          :
-          <Header
-            _func={(e: any) => searchUser(e)}
-            searchBarInput={true}
-            onOpenDrawer={() => navigation.dispatch(DrawerActions.openDrawer())} />
-        }
+          _func={(e: any) => searchUser(e)}
+          searchBarInput={true}
+          onOpenDrawer={() => navigation.dispatch(DrawerActions.openDrawer())} />
+
         <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
 
           <HeaderTitle>
@@ -123,7 +151,7 @@ export const Shop: React.FC = ({ navigation }: any) => {
           {isLoader ?
             <ActivityIndicator
               style={{ marginTop: "50%" }}
-              size="small" color={'#ffffff'}
+              size="small" color={'black'}
             /> :
             Subcatogery.length > 0 && Subcatogery.map((v: any, i) => {
               // i === 5 && console.log(v, "sub catogeris in render")

2c6611df6f1a69814f6685cfee5bdd44bf73200c
Muzammil shah <mynameismuzammilhussainshah@gmail.com>
2021-09-04 15:50:13 +0500

    icon issue fix

--- drawerFeature/contactUs/ContactUs.tsx
+++ drawerFeature/contactUs/ContactUs.tsx
@@ -62,7 +62,7 @@ export const ContactUs: React.FC = () => {
   }
 
 
-  const dialCall = (tel:any) => {
+  const dialCall = (tel: any) => {
 
     let phoneNumber = '';
 
@@ -82,13 +82,13 @@ export const ContactUs: React.FC = () => {
       <View style={{ flex: 1.5, backgroundColor: Colors.black, justifyContent: "center" }}>
         <Header RatingScreen={true} onOpenDrawer={() => navigation.dispatch(DrawerActions.openDrawer())} />
       </View>
-      <View style={{ flex: 8.5, backgroundColor: Colors.titleGray, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ flex: 8.5, overflow: "hidden", backgroundColor: Colors.titleGray, justifyContent: "center", alignItems: "center" }}>
         {getcontactUsInfo?.lat &&
           <GoogleStaticMap
             latitude={getcontactUsInfo.lat}
             longitude={getcontactUsInfo.lng}
             zoom={8}
-            size={{ width: 450, height: 550 }}
+            size={{ width: 450, height: 700 }}
             apiKey={GoogleMapKey}
           />
         }
@@ -103,13 +103,13 @@ export const ContactUs: React.FC = () => {
             <View style={{ flex: 3.5, alignItems: "center", justifyContent: "space-around" }}>
               <Text>{getcontactUsInfo && getcontactUsInfo.email}</Text>
               <View>
-                <TouchableOpacity onPress={()=>{dialCall(getcontactUsInfo.tel)}}>
+                <TouchableOpacity onPress={() => { dialCall(getcontactUsInfo.tel) }}>
                   <Text style={{ color: Colors.darkGray }}>{getcontactUsInfo && "+ " + getcontactUsInfo.tel}</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=>{dialCall(getcontactUsInfo.mobile1)}}>
+                <TouchableOpacity onPress={() => { dialCall(getcontactUsInfo.mobile1) }}>
                   <Text style={{ color: Colors.darkGray }}>{getcontactUsInfo && "+ " + getcontactUsInfo.mobile1}</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=>{dialCall(getcontactUsInfo.mobile2)}}>
+                <TouchableOpacity onPress={() => { dialCall(getcontactUsInfo.mobile2) }}>
                   <Text style={{ color: Colors.darkGray }}>{getcontactUsInfo && "+ " + getcontactUsInfo.mobile2}</Text>
                 </TouchableOpacity>
               </View>
@@ -117,38 +117,48 @@ export const ContactUs: React.FC = () => {
             </View>
             <View style={{ flex: 1.3, flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
               <TouchableOpacity
+                style={{ height: '45%',marginHorizontal:2, width: 30 }}
+
                 onPress={() => {
                   navigation.navigate("webView", { link: getcontactUsInfo && getcontactUsInfo.facebook })
                 }}
                 activeOpacity={0.7}>
-                <FastImage style={{ width: 40, height: "60%" }} source={require('../../assets/ContactUs/Facebook.png')} resizeMode={FastImage.resizeMode.contain} />
+                <FastImage style={{ width: "100%", height: "100%" }} source={require('../../assets/ContactUs/Facebook.png')} resizeMode={FastImage.resizeMode.contain} />
               </TouchableOpacity>
               <TouchableOpacity
+                style={{ height: '60%',marginHorizontal:2, width: 35 }}
+
                 onPress={() => {
                   navigation.navigate("webView", { link: getcontactUsInfo && getcontactUsInfo.instagram })
                 }}
                 activeOpacity={0.7}>
-                <FastImage style={{ width: 40, height: "60%" }} source={require('../../assets/ContactUs/insta.png')} resizeMode={FastImage.resizeMode.contain} />
+                <FastImage style={{ width: "100%", height: "100%" }} source={require('../../assets/ContactUs/insta.png')} resizeMode={FastImage.resizeMode.contain} />
               </TouchableOpacity>
               <TouchableOpacity
+                style={{ height: '70%',marginHorizontal:2, width: 37 }}
+
                 onPress={() => {
                   navigation.navigate("webView", { link: getcontactUsInfo && getcontactUsInfo.youtube })
                 }}
                 activeOpacity={0.7}>
-                <FastImage style={{ width: 40, height: "60%" }} source={require('../../assets/ContactUs/youtube.png')} resizeMode={FastImage.resizeMode.contain} />
+                <FastImage style={{ width: "100%", height: "100%" }} source={require('../../assets/ContactUs/youtube.png')} resizeMode={FastImage.resizeMode.contain} />
               </TouchableOpacity>
               <TouchableOpacity
+                style={{ height: '55%',marginHorizontal:2, width: 35 }}
+
                 onPress={() => {
                   navigation.navigate("webView", { link: getcontactUsInfo && getcontactUsInfo.pinterest })
                 }}
                 activeOpacity={0.7}>
-                <FastImage style={{ width: 40, height: "60%" }} source={require('../../assets/ContactUs/print.png')} resizeMode={FastImage.resizeMode.contain} />
+                <FastImage style={{ width: "100%", height: "100%" }} source={require('../../assets/ContactUs/print.png')} resizeMode={FastImage.resizeMode.contain} />
               </TouchableOpacity>
               <TouchableOpacity
+                style={{ height: '55%',marginHorizontal:2, width: 35 }}
+
                 onPress={() => {
                   navigation.navigate("webView", { link: getcontactUsInfo && getcontactUsInfo.tiktok })
                 }} activeOpacity={0.7}>
-                <FastImage style={{ width: 40, height: "60%" }} source={require('../../assets/ContactUs/tiktok.png')} resizeMode={FastImage.resizeMode.contain} />
+                <FastImage style={{ width: "100%", height: "100%" }} source={require('../../assets/ContactUs/tiktok.png')} resizeMode={FastImage.resizeMode.contain} />
               </TouchableOpacity>
             </View>
             <TouchableOpacity onPress={() => { openGoogleMapDirection() }}

931628eabcf38a98668eb7cf56815fa7531fe14f
muzammil hussain shah <mynameismuzammilhussainshah@gmail.com>
2021-09-27 23:28:06 +0500

    privacy policy scrolling

--- drawerFeature/PrivacyPolicy/privacyScreen.tsx
+++ drawerFeature/PrivacyPolicy/privacyScreen.tsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 
 import { useNavigation, DrawerActions, } from '@react-navigation/native';
 
-import { _getPrivacyPolicy } from '../../store/action/companyAction';
+import { _getPrivacyPolicy, } from '../../store/action/companyAction';
 
-import { useWindowDimensions } from "react-native"
+import { useWindowDimensions,ScrollView } from "react-native"
 
 import RenderHTML from "react-native-render-html";
 
@@ -50,12 +50,15 @@ export const privacyScreen: React.FC = () => {
         <Header RatingScreen={true} onOpenDrawer={() => navigation.dispatch(DrawerActions.openDrawer())} />
       </View>
       <View style={{ flex: 8.5, backgroundColor: Colors.titleGray, }}>
+      <ScrollView>
         <Text style={{ fontSize: 20, color: Colors.darkGray, margin: 20 }}>Privacy Policy</Text>
         <View style={{ marginHorizontal: 20 }}>
           {companyPolicy && companyPolicy.terms && companyPolicy.terms.length > 0 &&
             <RenderHTML contentWidth={width} source={{ html }} />
           }
         </View>
+
+      </ScrollView>
       </View>
     </View>
   )
